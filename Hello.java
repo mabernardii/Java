97ff87f9508b38fc0841425efe2fbbf6c673676f
mabernardii <manuela.bernardi1a@gmail.com>
2025-03-26 10:36:30 -0300

    Meu primeiro projeto java

---
+++ Hello.java
@@ -0,0 +1,11 @@
+/******************************************************************************
+Meu primeiro programa - Hello World
+
+*******************************************************************************/
+
+public class Hello
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
