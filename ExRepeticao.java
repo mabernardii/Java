954e7777ff5767cdf88727251a8ae352aee8c100
mabernardii <manuela.bernardi1a@gmail.com>
2025-04-02 08:12:50 -0300

    Exemplo de estrutura de repetição

---
+++ ExRepeticao.java
@@ -0,0 +1,14 @@
+/******************************************************************************
+Criar um exemplo de estrutura de repetição
+*******************************************************************************/
+import java.util.Scanner;
+
+public class ExRepeticao {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        for (int i = 0; i <=5; i++) {
+        System.out.println ("A variável i agora vale: " +i);   
+
+        }
+    }
+}
