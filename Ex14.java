bf3b2f89422311d5f7bba527ec4e29de8ad133cf
mabernardii <manuela.bernardi1a@gmail.com>
2025-04-02 08:32:30 -0300

    Exercício 14 - Exiba 10x a mensagem EU GOSTO DE ESTUDAR ALGORITIMOS

---
+++ Ex14.java
@@ -0,0 +1,15 @@
+/******************************************************************************
+14. Escreva um programa que exiba 10 vezes a mensagem “Eu gosto de estudar Algoritmos!”.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Ex14
+
+{
+      public static void main(String[] args) {
+      Scanner ler = new Scanner (System.in);
+      
+      for (int i=0 ; i<= 10; i++)
+      System.out.println("Eu gosto de estudar algoritmos!");
+    }
+}
