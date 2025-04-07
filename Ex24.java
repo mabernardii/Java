01eda99f0462024b4bc7e1b21d943ed9ce674059
Aluno <manuela.bernardi1a@gmail.com>
2025-04-07 10:41:32 -0300

    Calcule a soma dos números de 1 á 10

---
+++ Ex24.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+Ex24. Soma de números de 1 a 10 = 1+2+3+4...+10
+*******************************************************************************/
+
+public class Ex24
+{
+	public static void main(String[] args) {
+		int soma = 0;
+		int numero =1;
+		
+		while (numero<=10 ) {
+		    soma += numero;
+		    numero++;
+		}
+		
+		System.out.println ("A soma dos números de 1 a 10 é:" + soma);
+	}
+}
+
