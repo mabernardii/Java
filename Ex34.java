448ab13882e2a60f53389d16cb8ae06ca5acb181
Aluno <manuela.bernardi1a@gmail.com>
2025-04-07 15:09:50 -0300

    Exercício 34 - Ex de estrutura de repetição usando do while
    git status

---
+++ Ex34.java
@@ -0,0 +1,15 @@
+/******************************************************************************
+Ex34. Exemplo de estrutura de repetição DOWHILE
+*******************************************************************************/
+import java.util.Scanner;
+public class Ex34
+{
+	public static void main(String[] args) {
+		int contador = 0;
+		
+		do {
+		    System.out.println (contador);
+		    contador ++;
+		} while (contador <=5);
+	}
+}
